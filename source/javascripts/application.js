02f131599f3e59439a1a7267ca28bfa920aa1828
Orta Therox <orta.therox@gmail.com>
2013-11-10 18:00:08 -0500

    initial work on the front page info

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,6 +2,11 @@
 *= require jquery.min
 *= require modernizr.min
 *= require bootstrap.min
+
+*= require history.min.js
+*= require history.adapter.jquery.min.js
+*= require picky.min.js
+*= require search.config.js
 */
 
 
