0e24f69b14ecc26e351d657acecb34b667aa6e95
Swizzlr <git@swizzlr.co>
2014-01-27 10:17:56 +0000

    refactor to new file

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -7,6 +7,7 @@
 *= require history.adapter.jquery.min.js
 *= require picky.min.js
 *= require search.config.js
+*= require cocoapush.js
 *= require zero-clipboard.min.js
 *= require has_flash.js
 *= require ICanHaz.js
@@ -21,14 +22,14 @@ $(document).bind("touchmove", function(e){
 
 $( document ).ready( function(){
   $('.underscore a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-    
+
     var index = $(e.target.parentElement).index()
     var width = $(window).width();
     var constant = (width < 768) ? 33: 40;
     var percent = (index * constant).toString()
     $("#homepage-tab-indicator").css("margin-left", percent + "%");
   })
- 
+
   if( hasFlash() ) {
     $("html").addClass("flash")
   }
