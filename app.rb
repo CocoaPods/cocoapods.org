31ba37b300dc0b4ee675fd28aa7dfb92c914276f
Florian R. Hanke <florian.hanke@gmail.com>
2014-09-13 14:58:16 +0200

    Move middleman stuff into a middleman folder.

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+
+class App < Sinatra::Base
+
+  # TODO Add links to statically generated code.
+  #
+  
+  
+  # Methods for the dynamic part.
+  #
+  
+  #
+  #
+  get '/pods/:name' do
+    
+  end
+  
+end
---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'bundler/setup'
+
+$:.unshift File.expand_path('..', __FILE__)
+require 'app'
+
+run App
