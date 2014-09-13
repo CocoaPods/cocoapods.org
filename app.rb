30c517da21a777f326b1006a8dc47fed9c512c9e
Florian R. Hanke <florian.hanke@gmail.com>
2014-09-13 15:31:57 +0200

    Add comfortable accessors for the DB domain.

--- app.rb
+++ app.rb
@@ -6,9 +6,18 @@ class App < Sinatra::Base
   #
   set :public_folder, File.dirname(__FILE__) + '/middleman/build'
   
+  # Set up dynamic part.
+  #
+  require_relative 'domain'
   
   # Methods for the dynamic part.
   #
+  DB.entities.each do |entity|
+    name = entity.plural
+    define_method name do
+      DB[name]
+    end
+  end
   
   #
   #
