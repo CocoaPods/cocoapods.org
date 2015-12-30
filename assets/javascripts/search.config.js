85104bc5238b83e09f629f9427e43b3019539a03
Orta Therox <orta.therox@gmail.com>
2015-12-30 23:05:35 +0000

    Undo #239

--- assets/javascripts/search.config.js
+++ assets/javascripts/search.config.js
@@ -451,10 +451,9 @@ $(window).ready(function() {
 
       // Render the JSON into HTML.
       //
-      // Note: we unique because of #226
       var allocations = data.allocations;
       allocations.each(function(i, allocation) {
-        allocation.entries = jQuery.unique(allocation.entries).map(function(i, entry) {
+        allocation.entries = allocation.entries.map(function(i, entry) {
           return render(entry);
         });
       });
